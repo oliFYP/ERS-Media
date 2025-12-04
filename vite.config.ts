bcf4b428853f4f74ead5eff16df4fc4c36cbb431
Olivier Smith <oliviersmith@Oliviers-MacBook-Pro.local>
2025-12-04 13:53:41 -0500

    changed vite.config file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react"; // or svelte/vue if you use them
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+  base: "/ERS-Media/",
 });
